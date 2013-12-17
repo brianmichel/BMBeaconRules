38e9ab8c5d22a4a4aaa41dce2534d29678c57fb8
Brian Michel <bmichel@urbn.com>
2013-12-17 08:45:13 -0500

    adding more comments

--- BMBeaconRules/Classes/BMProximityRule.h
+++ BMBeaconRules/Classes/BMProximityRule.h
@@ -8,13 +8,35 @@
 
 #import "BMBeaconRules.h"
 
+@protocol BMProximity <NSObject>
+
+/**
+ Called when the user has entered the specified activationProximity
+ 
+ @note Subclassers should NOT call super, this will end up triggering
+ the activateRule: logic for this baseclass. I'd love to figure out a
+ cleaner way to do this, I'm open to ideas...
+ */
+- (void)didEnterProximity;
+
+/**
+ Called when the user has exited the specified activationProximity
+ 
+ @note Subclassers should NOT call super, this will end up triggering
+ the activateRule: logic for this baseclass. I'd love to figure out a
+ cleaner way to do this, I'm open to ideas...
+ */
+- (void)didExitProximity;
+
+@end
+
 /**
     This rule will activate when beacons in the appropriate region
  come into the associated proximity zone. It will also toggle itself
  off if the listening device (phone/pad) is moved out of the required 
  proximity.
  */
-@interface BMProximityRule : BMBeaconRule
+@interface BMProximityRule : BMBeaconRule <BMProximity>
 
 /**
  The proximity in which the rule should activate
@@ -42,23 +64,5 @@
  @warning if promximity is passed as CLProximityUnknown range monitoring will be disabled.
  */
 - (instancetype)initWithRegion:(CLBeaconRegion *)region activationProximity:(CLProximity)activationProximity andCallback:(BMBeaconRuleCallback)callback;
-
-/**
- Called when the user has entered the specified activationProximity
- 
- @note Subclassers should NOT call super, this will end up triggering
- the activateRule: logic for this baseclass. I'd love to figure out a
- cleaner way to do this, I'm open to ideas...
- */
-- (void)didEnterProximity;
-
-/**
- Called when the user has exited the specified activationProximity
- 
- @note Subclassers should NOT call super, this will end up triggering
- the activateRule: logic for this baseclass. I'd love to figure out a
- cleaner way to do this, I'm open to ideas...
- */
-- (void)didExitProximity;
 
 @end
---
+++ BMBeaconRulesTests/BMProximityRuleTest.m
@@ -0,0 +1,34 @@
+//
+//  BMBeaconRules - BMProximityRuleTest.m
+//  Copyright 2013 Brian Michel. All rights reserved.
+//
+//  Created by: Brian Michel
+//
+
+    // Class under test
+#import "BMProximityRule.h"
+
+    // Collaborators
+
+    // Test support
+#import <XCTest/XCTest.h>
+
+// Uncomment the next two lines to use OCHamcrest for test assertions:
+//#define HC_SHORTHAND
+//#import <OCHamcrestIOS/OCHamcrestIOS.h>
+
+// Uncomment the next two lines to use OCMockito for mock objects:
+//#define MOCKITO_SHORTHAND
+//#import <OCMockitoIOS/OCMockitoIOS.h>
+
+
+@interface BMProximityRuleTest : XCTest
+@end
+
+@implementation BMProximityRuleTest
+{
+    // test fixture ivars go here
+}
+
+
+@end
